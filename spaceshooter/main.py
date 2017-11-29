ee3156869f94924b32fd595e74974869c85bbc9e
Henry Williams <henry.williams@auckland.ac.nz>
2017-11-29 20:54:30 +1300

    Resized game screen to be sqaure and 10x the resized 80x80 image

--- spaceshooter/main.py
+++ spaceshooter/main.py
@@ -8,7 +8,7 @@ import cv2
 pygame.init()
  
 display_width = 800
-display_height = 600
+display_height = 800
  
 black = (0,0,0)
 white = (255,255,255)
@@ -18,10 +18,6 @@ green = (0,200,0)
 
 bright_red = (255,0,0)
 bright_green = (0,255,0)
- 
-block_color = (53,115,255)
- 
-car_width = 73
  
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('Spaceshooter')
